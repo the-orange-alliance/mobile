1747883e929aa1f93427dbbe6df10921003621aa
Travis Weir <tweirtx@gmail.com>
2019-05-30 17:08:26 -0500

    Start publishing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:toa_flutter/Main.dart';
+import 'package:toa_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
