4657c18597fef3ab44376ed2360598751baa2868
Travis Weir <tweirtx@gmail.com>
2019-12-19 17:31:59 -0600

    Move credential files before building

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_29727b427600_key -iv $encrypted_29727b427600_iv -in credentials.tar.xz.enc -out credentials.tar.xz -d
-tar -xf credentials.tar.xz
-mv android-keystore.jks android/app
-mv key.properties android
-mv google-services.json android/app
-mv GoogleService-Info.plist ios/Runner
-mv api-9048300026487180911-124804-e70b22003165.json android
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]
 then
   cd ios && fastlane release
